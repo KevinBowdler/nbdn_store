b508bca9bc582d09f74f48975a21c5665e3dd4b0
Kevin Bowdler <github@registered.me.uk>
2010-09-02 16:20:12 +0100

    Test

---
+++ product/nothinbutdotnetstore/catagorisations/ConcreteImplementation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace nothinbutdotnetstore.catagorisations
+{
+    public class  ConcreteImplementation : Attribute
+    {
+        private Attribute attribute;
+
+        public ConcreteImplementation(Attribute attribute)
+        {
+            this.attribute = attribute;
+        }
+    }
+}
---
+++ product/nothinbutdotnetstore/catagorisations/GatewayPattern.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace nothinbutdotnetstore.catagorisations
+{
+    class GatewayPattern : Attribute
+    {
+    }
+}
